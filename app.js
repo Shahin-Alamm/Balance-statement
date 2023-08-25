72f2cf9c80b720c364b059a834e01b5f4f67b7d4
shahin-alamm <shahinalamr27@gmail.com>
2023-08-25 09:38:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const input = document.getElementById('income')
+const food = document.getElementById('food')
+const cloths = document.getElementById('cloths')
+const rent = document.getElementById('rent')
+
+const addButton = document.getElementById('addBtn');
+
+addButton.addEventListener('click',function(){
+const inputBalance = parseFloat(input.value) //ata must sobar upore dite hobe 
+const balance = document.getElementById('balance');
+
+    const foodInput = parseFloat(food.value)
+    const clothInput = parseFloat(cloths.value)
+    const rentInput = parseFloat(rent.value)
+
+    const totalExpense = foodInput + clothInput + rentInput;
+    document.getElementById("expense-balance").innerText = totalExpense
+
+    const totalBalance = inputBalance - totalExpense;
+    balance.innerText = totalBalance;
+
+    const savingAmount = totalBalance + .20 ;
+    // document.getElementById("saving-amount").innerText = savingAmount;
+
+    document.getElementById("remain-balance").innerText =savingAmount;
+})
+
+
+
+
+// let a = 10000;
+// if(a<20000){
+//     document.getElementById('error-msg').classList.remove('d-none')
+// }
